d2481c70c3dafd15a980f68c113cb8064758fa6f
Freewind <pli@thoughtworks.com>
2018-01-26 20:34:35 +0800

    Include scala/html and jquery demo

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "Scala Hello World Demo"
+name := "ScalaJS Hello World Demo"
 
 version := "0.1"
 
@@ -8,6 +8,19 @@ scalaVersion := "2.12.4"
 
 sbtVersion := "1.0.4"
 
+enablePlugins(ScalaJSPlugin)
+
+// This is an application with a main method
+scalaJSUseMainModuleInitializer := true
+
+skip in packageJSDependencies := false
+
 libraryDependencies ++= Seq(
+  "org.scala-js" %%% "scalajs-dom" % "0.9.1",
+  "be.doeraene" %%% "scalajs-jquery" % "0.9.1",
   "org.scalatest" %% "scalatest" % "3.0.4" % "test"
+)
+
+jsDependencies ++= Seq(
+  "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
 )
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,32 @@
 package example
 
-object Hello extends App {
+import org.scalajs.dom
+import dom.document
+import org.scalajs.jquery.jQuery
+
+object Hello {
 
   def hello(name: String) = s"Hello, $name!"
 
-  println(hello("Scala"))
+  def main(args: Array[String]): Unit = {
+    println(hello("Scala in console"))
+    jQuery(() => setupUI())
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
+  def addClickedMessage(): Unit = {
+    jQuery("body").append("<p>You clicked the button!</p>")
+  }
+
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(() => addClickedMessage())
+    jQuery("body").append(s"<p>${hello("Scala in page")}</p>")
+  }
 
 }
